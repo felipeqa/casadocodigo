a188cde7846ecda02d7ad12cf00dda0190adc66a
Felipe Rodrigues <feliperodrigues@MacBook-Pro-de-Felipe.local>
2020-03-06 15:11:52 -0300

    implementando JPA Config

--- src/main/java/br/com/casadocodigo/loja/controllers/ProdutosController.java
+++ src/main/java/br/com/casadocodigo/loja/controllers/ProdutosController.java
@@ -1,11 +1,16 @@
 package br.com.casadocodigo.loja.controllers;
 
+import br.com.casadocodigo.loja.daos.ProdutoDAO;
 import br.com.casadocodigo.loja.models.Produto;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class ProdutosController {
+
+    @Autowired
+    private ProdutoDAO produtoDao;
 
     @RequestMapping("/produtos/form")
     public String form(){
@@ -15,7 +20,7 @@ public class ProdutosController {
     @RequestMapping("/produtos")
     public String grava(Produto produto){
         System.out.println(produto.toString()  );
-
+         produtoDao.gravar(produto);
          return "produtos/ok";
     }
 
