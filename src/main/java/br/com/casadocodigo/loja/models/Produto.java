a25c63da7f07cce4200da2d1ae43132b62a33850
Felipe Rodrigues <feliperodrigues@MacBook-Pro-de-Felipe.local>
2020-03-09 18:59:40 -0300

    associando preço ao produto

--- src/main/java/br/com/casadocodigo/loja/models/Produto.java
+++ src/main/java/br/com/casadocodigo/loja/models/Produto.java
@@ -1,9 +1,7 @@
 package br.com.casadocodigo.loja.models;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.List;
 
 @Entity
 public class Produto {
@@ -14,6 +12,9 @@ public class Produto {
     private String titulo;
     private String descricao;
     private int paginas;
+
+    @ElementCollection
+    private List<Preco> precos;
 
     public String getTitulo() {
         return titulo;
@@ -37,6 +38,22 @@ public class Produto {
 
     public void setPaginas(int paginas) {
         this.paginas = paginas;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public List<Preco> getPrecos() {
+        return precos;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setPrecos(List<Preco> precos) {
+        this.precos = precos;
     }
 
     @Override
